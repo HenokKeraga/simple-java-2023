1cd702ec81f1d1bd05890e7cd70c217366d9cfdb
henok keraga <henokkeraga@gmail.com>
2023-04-23 08:05:50 -0600

    java

---
+++ simple-java/vscode/Hello.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+public class Hello {
+ 	public static void main(String [] args){
+
+        var scan = new Scanner(System.in);
+         int a =scan.nextInt();
+         int  b = scan.nextInt();
+ 
+     	 System.out.println((a+b)/2);
+
+
+
+	}
+
+
+}
